36e388e84b99eda71e37ec0d17eb7010814371f8
Rita Martinho <up201709727@fe.up.pt>
2019-11-15 17:52:57 +0000

    let's start

---
+++ Lab_work2/downloadftp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <netdb.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
