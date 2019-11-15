f8f9973856766fdd064dd1a1d9a326b0c72902ff
Rita Martinho <up201709727@fe.up.pt>
2019-11-15 19:46:49 +0000

    lets start

--- Lab_work2/downloadftp.c
+++ Lab_work2/downloadftp.c
@@ -11,3 +11,24 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+
+/* FTP WORKS ON APPLICATION LAYER! MAIN GOAL: MOVING FILES ( in our project we are supposed to only download a file)
+//we need to have two sockets: one for control and one for data
+
+/* main steps:
+
+- get user and password (maybe send this via argv?)
+- start connection using control socket
+- check permissions of the user
+- passive mode
+- start connection using data socket
+- retrieve - send a request for a specific file
+- get the file from the server
+- quit connection
+- */
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
